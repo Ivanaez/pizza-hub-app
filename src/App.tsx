0a82547e2ba3c6d06841885b036d709c3e66c568
Ivanaez <ivan.evtimov11@gmail.com>
2026-03-16 11:23:26 +0100

    fix: normalize folder casing for linux build

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import HomePage from "./pages/Home/HomePage";
-import LoginPage from "./pages/Login/LoginPage";
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/login/LoginPage";
 
 function App() {
   return (
