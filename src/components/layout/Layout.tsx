6d9f08a74d550a85504bfb371a9dbd85ce2f92c6
Ivanaez <ivan.evtimov11@gmail.com>
2026-04-25 21:34:34 +0200

    feat(layout): add global cart toast notification

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,24 +1,37 @@
 import { Outlet } from "react-router-dom";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
-
-
+import { useCart } from "../../features/cart/CartContext";
+import styles from "./Layout.module.css";
 
 // Main layout wrapper for all pages that use header and footer
 function Layout() {
+  
+// Get toast message from cart context to show global notifications
+  const { toastMessage } = useCart();  
+
   return (
                     // This layout will be used for all pages that need header and footer
-    <div className="appLayout">
 
-       {/* Global Header */}
+
+ <div className="appLayout">
+
+  {/* Global cart toast notification */}
+{toastMessage && (
+  <div className={styles.cartToast}>
+    {toastMessage}
+  </div>
+)}
+   
+       {/* Global Header ****************************/}
       <Header />
-
-        {/* Main content of the page */}
+ 
+        {/* Main content of the page ***************************/}
       <main className="appMain">
         <Outlet />
       </main>
       
-      {/* Global Footer */}
+      {/* Global Footer ****************************/}
       <Footer />
 
       </div>
