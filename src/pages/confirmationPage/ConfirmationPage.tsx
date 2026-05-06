4101657e22d68f4d2636c66783b4cc83f9202268
Ivanaez <ivan.evtimov11@gmail.com>
2026-05-06 14:45:28 +0200

    feat(confirmation-page): add basic confirmation page

---
+++ src/pages/confirmationPage/ConfirmationPage.tsx
@@ -0,0 +1,10 @@
+import styles from "./ConfirmationPage.module.css"
+
+export default function ConfirmationPage() {
+  return (
+    <main className={styles.confirmationPage}>
+      <h1>Order Confirmed</h1>
+      <p>Thank you for your order!</p>
+    </main>
+  )
+}
