c60734f013e3f6484684ce0aa75d6c6005b30ee2
Ivanaez <ivan.evtimov11@gmail.com>
2026-05-18 16:56:34 +0200

    feat(main): wrap app with UserProvider for global user state management

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-
+import { UserProvider } from "./features/user/UserContext";
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <CartProvider>
+         <UserProvider>
         <App />
+        </UserProvider>
       </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
