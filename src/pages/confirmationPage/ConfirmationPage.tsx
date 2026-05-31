d8700394d4d885a40cf330e67efe9b46bf6d72da
Ivanaez <ivan.evtimov11@gmail.com>
2026-05-31 17:18:29 +0200

    feat(confirmationPage): protect direct confirmation access

--- src/pages/confirmationPage/ConfirmationPage.tsx
+++ src/pages/confirmationPage/ConfirmationPage.tsx
@@ -1,6 +1,19 @@
 import styles from "./ConfirmationPage.module.css"
+import { Navigate, useLocation } from "react-router-dom";
+
+
+
 
 export function ConfirmationPage() {
+  
+ // Get navigation state
+const location = useLocation();
+
+// Prevent direct access
+if (!location.state) {
+  return <Navigate to="/checkout" replace />;
+}
+
   return (
     <main className={styles.confirmationPage}>
       <h1>Order Confirmed</h1>
