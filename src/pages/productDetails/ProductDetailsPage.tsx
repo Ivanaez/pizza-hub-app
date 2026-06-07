b023e217eae388ed15f7484bc71ca11f14a986bd
Ivanaez <ivan.evtimov11@gmail.com>
2026-06-07 20:20:11 +0200

    feat(product-details-page): add product details page

--- src/pages/productDetails/ProductDetailsPage.tsx
+++ src/pages/productDetails/ProductDetailsPage.tsx
@@ -1,13 +1,13 @@
 import { CategoryCarousel } from "@/components/category/CategoryCarousel/CategoryCarousel";
+import { ProductDetails } from "@/components/product/ProductDetails/ProductDetails";
 import styles from "./ProductDetailsPage.module.css";
 
-// Product details page
+/* Product details page shell */
 export function ProductDetailsPage() {
   return (
-    // Product details page layout
     <main className={styles.page}>
       <CategoryCarousel />
-      {/* Product details content will be added here */}
+      <ProductDetails />
     </main>
   );
 }
