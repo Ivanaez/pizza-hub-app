21e55b25a3a12fb300f665e0eac420d4a8e692ea
Ivanaez <ivan.evtimov11@gmail.com>
2026-06-21 14:47:08 +0200

    feat(homepage): add HomeDeals section to homepage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,6 @@
 import styles from "./HomePage.module.css";
 import { HomeHero } from "@/components/home/HomeHero/HomeHero";
+import { HomeDeals } from "@/components/home/HomeDeals/HomeDeals";
 import { ProductCarousel } from "@/components/product/ProductCarousel/ProductCarousel";
 import { UserGreeting } from "@/components/user/UserGreeting";
 
@@ -10,8 +11,13 @@ export function HomePage() {
       {/* Home hero */}
       <HomeHero />
 
+    
       {/* Popular products */}
       <ProductCarousel />
+
+
+     {/* Home deals */}
+      <HomeDeals />
 
       {/* User greeting */}
       <UserGreeting />
